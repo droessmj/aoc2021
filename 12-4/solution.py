5a51f3e53f97e4360f6aaa59ae40427b611b089c
droessmj <droessmj@gmail.com>
2021-12-04 10:14:49 -0600

    working pt 1

--- 12-4/solution.py
+++ 12-4/solution.py
@@ -5,8 +5,10 @@ class BingoBoard:
 
     def __init__(self, board):
         self.board = board
+        self.winning_number_sets = []
+        self.all_numbers_set = set()
 
-        for val_row in board:
+        for val_row in self.board:
             row_set = set()
             # row values as a set
             for entry in val_row:
@@ -15,8 +17,8 @@ class BingoBoard:
             self.winning_number_sets.append(row_set)
 
         # calc column value as a set
-        for col in range(len(board)):
-            col_set = { board[0][col], board[1][col], board[2][col], board[3][col], board[4][col] }
+        for col in range(len(self.board)):
+            col_set = { self.board[0][col], self.board[1][col], self.board[2][col], self.board[3][col], self.board[4][col] }
             self.winning_number_sets.append(col_set)
     
     def __str__(self):
@@ -25,10 +27,45 @@ class BingoBoard:
     def __repr__(self) -> str:
         return f"{self.board}\n"
 
-all_called_numbers = []
+    def check_for_winner(self, called_numbers):
+        if len(self.all_numbers_set.intersection(set(called_numbers))) < 5:
+            return False
+        else:
+            for s in self.winning_number_sets:
+                matches_count = len(s.intersection(set(called_numbers)))
+                if matches_count == 5:
+                    return True
+            return False
+    
+    def calc_score(self, called_numbers):
+        marked = self.all_numbers_set.intersection(set(called_numbers))
+        sum_marked = marked
+        sum_unmarked = sum(list(self.all_numbers_set.difference(marked)))
+        print(sum_marked)
+        print(called_numbers[-1])
+        return sum_unmarked * called_numbers[-1]
+
+def callNumber_returnIsWinner(called_numbers, all_bingo_boards):
+    # take the called number, check all boards for state
+    # if a winner is detected calc the return value
+    if len(called_numbers) < 5:
+        return False
+
+    for board in all_bingo_boards:
+        if board.check_for_winner(called_numbers):
+            print(board)
+            # calculate value
+            val = board.calc_score(called_numbers)
+            return val
+        
+    # return result as False or winning value
+    return False
+
+
+all_num_to_call = []
 all_bingo_boards = []
-with open("./test.txt") as f:
-    all_called_numbers = [int(i) for i in f.readline().split(",")]
+with open("./input.txt") as f:
+    all_num_to_call = [int(i) for i in f.readline().split(",")]
     f.readline()
 
     # build boards
@@ -44,9 +81,17 @@ with open("./test.txt") as f:
         idx += 1
 
         if idx > 4:
-            all_bingo_boards.append(BingoBoard(board_lines))
+            bb = BingoBoard(board_lines)
+            all_bingo_boards.append(bb)
             idx = 0
             board_lines = []
-            
 
-print(all_bingo_boards)
+call_idx = 0 
+called_numbers = []
+while True:
+    called_numbers.append(all_num_to_call[call_idx])
+    result = callNumber_returnIsWinner(called_numbers, all_bingo_boards)
+    if result: 
+        print(result)
+        break
+    call_idx += 1
