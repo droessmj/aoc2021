cafc00c2927f6c59a36a05eb9e62d10e16ae92fa
Michael Droessler <michael.droessler@lacework.net>
2021-12-10 09:01:03 -0600

    pt 2 day 10 wip

--- 12-9/solution.py
+++ 12-9/solution.py
@@ -1,7 +1,10 @@
 import sys
+from functools import reduce
+
+lookup = {}
 
 # return True if smaller than above/below
-def check_col(board,r,c, board_len, row_len):
+def check_col_smallest(board,r,c, board_len, row_len):
     if c == 0:  # can't check above
         return board[r][c] < board[r][c+1]
     elif c == row_len - 1: # can't check below
@@ -10,13 +13,45 @@ def check_col(board,r,c, board_len, row_len):
         return board[r][c] < board[r][c-1] and board[r][c] < board[r][c+1]
 
 # return True if smaller than left, right
-def check_row(board,r,c, board_len, row_len):
+def check_row_smallest(board,r,c, board_len, row_len):
     if r == 0:  # can't check left
         return board[r][c] < board[r+1][c]
     elif r == board_len - 1: # can't check right
         return board[r][c] < board[r-1][c]
     else: # left and right can be checked
         return board[r][c] < board[r-1][c] and board[r][c] < board[r+1][c]
+
+# return True if smaller than above/below
+def get_non_nine_neighbors(board,r,c, board_len, row_len):
+    possible_neighbors = [[0,1], [0,-1], [1,0], [-1,0]]
+    if r == 0: # skip check above
+        possible_neighbors.remove([-1,0])
+    if r == board_len -1: #skip check below
+        possible_neighbors.remove([1,0])
+    if c == 0: # skip check left
+        possible_neighbors.remove([0,-1])
+    if c == row_len -1: #skip check right
+        possible_neighbors.remove([0,1])
+
+    viable_neighbors = []
+    for neighbor in possible_neighbors: #TODO add a lookup table 
+        if board[r+neighbor[0]][c+neighbor[1]] != 9 and str(r+neighbor[0])+str(c+neighbor[1]) not in lookup:
+            viable_neighbors.append([r+neighbor[0],c+neighbor[1]])
+            lookup[str(r+neighbor[0])+str(c+neighbor[1])] = True
+    print(viable_neighbors)
+    return viable_neighbors
+
+def calc_basin_size(board,r,c,board_len,row_len):
+    basin_size = 1
+    # look to all four locations, add value to check if exists and not 9
+    viable_neighbors = get_non_nine_neighbors(board,r,c,board_len,row_len)
+    idx = 0
+    while idx < len(viable_neighbors):
+        basin_size += 1
+        viable_neighbors += get_non_nine_neighbors(board,viable_neighbors[idx][0],viable_neighbors[idx][1],board_len,row_len)
+        idx += 1
+    print( basin_size )
+    return basin_size
 
 in_file = 'test'
 
@@ -37,12 +72,24 @@ board_len = len(board)
 row_len = len(board[0])
 
 r = 0
+basin_points = []
 while r < board_len:
     c = 0
     while c < row_len:
-        if check_row(board,r,c,board_len,row_len) and check_col(board,r,c,board_len,row_len):
+        if check_row_smallest(board,r,c,board_len,row_len) and check_col_smallest(board,r,c,board_len,row_len):
             risk += 1 + int(board[r][c])
+            basin_points.append([r,c])
         c += 1
     r +=1 
 
-print(risk)
+basin_sizes = []
+for point in basin_points:
+    # check if adjacent points are in basin 
+    basin_size = calc_basin_size(board,point[0], point[1],board_len,row_len)
+    basin_sizes.append(basin_size)
+
+basin_sizes.sort()
+print(basin_sizes)
+largest_3 = basin_sizes[3::-1]
+
+print(reduce(lambda a, b: a * b, largest_3))
